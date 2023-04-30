b019b742d503b351f93c18a6cfecc2adb63110b8
Kevin Romero Peces-Barba <kevinrpb@amazon.es>
2023-04-30 12:15:11 +0200

    Initial Commit

---
+++ DartsScore/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DartsScore
+//
+//  Created by Romero Peces Barba, Kevin on 30/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DartsScore/DartsScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsScoreApp.swift
+//  DartsScore
+//
+//  Created by Romero Peces Barba, Kevin on 30/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DartsScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
